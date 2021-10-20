8b782a6d2ba17bdd4098c5562e9f8d172b78a82b
il-shunder <shunder.work@gmail.com>
2021-10-20 13:53:02 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,61 @@
+function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
+
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    }
+});
+
+const headerBurger = document.querySelector('.header__burger');
+const headerTopBody = document.querySelector('.header-top__body');
+if (headerBurger) {
+    headerBurger.addEventListener('click', () => {
+        headerBurger.classList.toggle('active');
+        headerTopBody.classList.toggle('active');
+        document.body.classList.toggle('body-lock');
+    });
+}
+
+const organizeBlockBtns = document.querySelectorAll('.organize-block__btn');
+if (organizeBlockBtns.length > 0) {
+    organizeBlockBtns.forEach((el) => {
+        el.addEventListener('click', () => {
+            const preview = el.parentNode.querySelector('.organize-block__preview');
+            const width = preview.clientWidth;
+
+            if (el.parentNode.classList.contains('active')) {
+                preview.style.transform = 'rotate(-180deg)';
+                el.style.transform = 'translateY(-50%)';
+                el.parentNode.classList.remove('active');
+            } else {
+                el.style.transform = `translateY(-50%) translate(${width}px, 0px)`;
+                preview.style.transform = `rotate(-180deg) translate(-${width}px, 0px)`;
+                el.parentNode.classList.add('active');
+            }
+        });
+    });
+}
+
+function organizeBtn(el) {
+    if (el.parentNode.classList.contains('active')) {
+        const preview = el.parentNode.querySelector('.organize-block__preview');
+        const width = preview.clientWidth;
+
+        el.style.transform = `translateY(-50%) translate(${width}px, 0px)`;
+        preview.style.transform = `rotate(-180deg) translate(-${width}px, 0px)`;
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (organizeBlockBtns.length > 0) {
+        organizeBlockBtns.forEach((el) => {
+            organizeBtn(el);
+        });
+    }
+});
